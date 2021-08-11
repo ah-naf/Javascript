b02dc4e550da1506c69861562d10adccfb4385a0
Ahnaf Hasan Shifat <81911439+ah-naf@users.noreply.github.com>
2021-08-11 18:50:55 +0600

    Add files via upload

---
+++ Quote Generator/app.js
@@ -0,0 +1,67 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let quotes = [];
+
+//Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+//Show new Quote
+function newQuote() {
+    loading();
+    // Pick random quote
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check Quote length to determine styling
+    if (quote.text.length > 80) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+//Get quote from API
+async function get() {
+    loading();
+    const url = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            quotes = await response.json();
+            newQuote();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+get();
